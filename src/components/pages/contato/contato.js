86676a360c338d410a86176dfd739e0927f05e61
Felipe Sander Dhein <felipesanderd2@gmail.com>
2024-05-17 23:33:02 -0300

    mudanças na página sobre

--- src/components/pages/contato/contato.js
+++ src/components/pages/contato/contato.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../header/header';
 
 function Contato() {
     return(
         <>
+        <Header />
         Entre em contato
         </>
     )
--- src/components/pages/sobre/sobre.js
+++ src/components/pages/sobre/sobre.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Header from '../../header/header';
+import './sobre.css';
+import ranchoEfino from '../../../assets/imgs/RANCHO-e-FINO CONFEITO.svg';
+
 
 function Sobre() {
     return(
         <>
-        Sobre nós
+            <Header />
+        <div className='contact-container'>
+            <div className='contact-content'>
+                <img id='rancho-fino' src={ ranchoEfino } alt='rancho-e-fino'></img>
+                <h1>Mas afinal, qual é a diferença entre o Rancho030 e a Fino Confeito?</h1>
+                <p>É o que muitas pessoas perguntam à nós.</p>
+            </div>
+
+            <div className='Fino-e-Rancho'>
+                <div className='FinoConfeito'>
+                    <h1>Fino Confeito</h1>
+                    <p>•É quem faz os doces</p>
+                    <p>•Uma marca que visa proporcionar doces e tortas maravilhosas</p>
+                </div>
+                <div className='Rancho030'>
+                    <h1>Rancho030</h1>
+                    <p>•É quem bota os doces para vender</p>
+                    <p>•Um espaço que visa proporcionar um ambiente inesquecível para quem o visita.</p>
+            </div>
+            </div>
+        </div>
         </>
     )
 }

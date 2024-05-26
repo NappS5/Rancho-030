119b1cc31b1c66d7cd43b9ccddeccf9609d2b3db
Felipe Sander Dhein <felipesanderd2@gmail.com>
2024-05-26 20:31:27 -0300

    video da página sobre

--- src/components/pages/sobre/sobre.js
+++ src/components/pages/sobre/sobre.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '../../header/header';
 import './sobre.css';
 import ranchoEfino from '../../../assets/imgs/RANCHO-FINO CONFEITO.png';
+import video from '../../../assets/videos/video-teste.mp4'
+import rodaCarreta from '../../../assets/imgs/roda-carreta.png'
+import rodaInvertida from '../../../assets/imgs/roda-carreta-virada.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
@@ -13,11 +16,12 @@ function Sobre() {
         <>
             <Header />
             <div className='video-container'>
-                <video></video>
+                <img alt='roda' id='roda-invertida' src={rodaInvertida}></img>
+                <video controls className='video-promocional' typeof='video/mp4' src={video}></video>
+                <img alt='roda' id='roda-carreta' src={rodaCarreta}></img>
             </div>
         <div className='about-container'>
             <div className='about-content'>
-                <img id='rancho-fino' src={ ranchoEfino } alt='rancho-e-fino'></img>
                 <h1>Mas afinal, qual é a diferença entre o Rancho030 e a Fino Confeito?</h1>
                 <p>É o que muitas pessoas perguntam à nós.</p>
             </div>
